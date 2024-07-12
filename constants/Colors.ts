624dd03d6acf04dcfb09e3a6a1d5baccc718ab59
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2024-07-12 13:19:10 +0100

    set color variables

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,38 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
 
 export default {
+  colors: {
+    primary: "#092C4C",
+    secondary: "#F2994A",
+    info: "#2F80ED",
+    success: "#27AE60",
+    warning: "#E2B93B",
+    error: "#EB5757",
+    errorAscent: "#EB5757",
+    black1: "#000000",
+    black2: "#1D1D1D",
+    black3: "#282828",
+    white: "#FFFFFF",
+    gray1: "#333333",
+    gray2: "#4F4F4F",
+    gray3: "#828282",
+    gray4: "#BDBDBD",
+    gray5: "#EOEOEO",
+  },
+
   light: {
-    text: '#000',
-    background: '#fff',
+    text: "#000",
+    background: "#fff",
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: "#fff",
+    background: "#000",
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorDark,
   },
 };
